f03a53fcf58fc1ad089062a470d31fa9e5958cbc
Sayan Chakrabarty <50846501+sayan-ch@users.noreply.github.com>
2025-01-16 12:35:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,23 +8,24 @@ document.addEventListener("DOMContentLoaded", () => {
             <section class="about-container">
                 <div class="about-text">
                     <h2>About Me</h2>
-                    <p>I am a Postdoctoral Research Fellow in Statistics at the University of Michigan, Ann Arbor. My research advisor is <a href="https://sites.google.com/umich.edu/elevina">link Dr. Liza Levina</a>. My research interests are in network analysis, bootstrapping, subsampling, conformal prediction, and uncertainty quantification. </p>
+                    <p>I am a Postdoctoral Research Fellow in Statistics at the University of Michigan, Ann Arbor. My postdoc advisor is <a href="https://sites.google.com/umich.edu/elevina">Dr. Liza Levina</a>. I received PhD in Statistics from The University of Illinois Urbana-Champaign. My PhD advisors were <a href="https://publish.illinois.edu/yuguo/">Dr. Yuguo Chen</a> and <a href="https://sites.google.com/ncsu.edu/srijansengupta">Dr. Srijan Sengupta</a>. 
+                    My research interests are in network analysis, bootstrapping, subsampling, conformal prediction, and uncertainty quantification. </p>
                 </div>
                 <div class="about-image">
                     <img src="profile-pic.jpg" alt="Your Photo" class="profile-pic">
                 </div>
             </section>
         `,
-        education: `
-            <section class="tab-content">
-                <h2>Education</h2>
-                <ul>
-                    <li><strong>University of Illinois Urbana-Champaign</strong> (2018-2024) - PhD in Statistics. Advisors: <a href="https://publish.illinois.edu/yuguo/">link Dr. Yuguo Chen</a> and <a href="https://sites.google.com/ncsu.edu/srijansengupta">link Dr. Srijan Sengupta</a> </li>
-                    <li><strong>Indian Statistical Institute</strong> (2016-2018) - M.Stat in Statistics. Thesis Advisor: <a href="https://www.isical.ac.in/~probal/">link Dr. Probal Chaudhuri</a> </li>
-                    <li><strong>St. Xavier’s College</strong> (2013-2016) - B.Sc. in Statistics</li>
-                </ul>
-            </section>
-        `,
+        // education: `
+        //     <section class="tab-content">
+        //         <h2>Education</h2>
+        //         <ul>
+        //             <li><strong>University of Illinois Urbana-Champaign</strong> (2018-2024) - PhD in Statistics. Advisors: <a href="https://publish.illinois.edu/yuguo/">link Dr. Yuguo Chen</a> and <a href="https://sites.google.com/ncsu.edu/srijansengupta">link Dr. Srijan Sengupta</a> </li>
+        //             <li><strong>Indian Statistical Institute</strong> (2016-2018) - M.Stat in Statistics. Thesis Advisor: <a href="https://www.isical.ac.in/~probal/">link Dr. Probal Chaudhuri</a> </li>
+        //             <li><strong>St. Xavier’s College</strong> (2013-2016) - B.Sc. in Statistics</li>
+        //         </ul>
+        //     </section>
+        // `,
         research: `
             <section class="tab-content">
                 <h2>Research</h2>
@@ -37,23 +38,23 @@ document.addEventListener("DOMContentLoaded", () => {
                 </ul>
             </section>
         `,
-        experience: `
-            <section class="tab-content">
-                <h2>Experience</h2>
-                <h3>Professional</h3>
-                <p>Consultant at Illinois Statistics Office, providing statistical consulting and analysis.</p>
-                <h3>Teaching</h3>
-                <p>Instructor and teaching assistant for courses such as STAT 200, STAT 400, and more.</p>
-            </section>
-        `,
-        contact: `
-            <section class="tab-content">
-                <h2>Contact</h2>
-                <p>Email: <a href="mailto:sayanc3@illinois.edu">sayanc3@illinois.edu</a></p>
-                <p>Email: <a href="mailto:sayanc2012@gmail.com">sayanc2012@gmail.com</a></p>
-            </section>
-        `
-    };
+        // experience: `
+        //     <section class="tab-content">
+        //         <h2>Experience</h2>
+        //         <h3>Professional</h3>
+        //         <p>Consultant at Illinois Statistics Office, providing statistical consulting and analysis.</p>
+        //         <h3>Teaching</h3>
+        //         <p>Instructor and teaching assistant for courses such as STAT 200, STAT 400, and more.</p>
+        //     </section>
+        // `,
+    //     contact: `
+    //         <section class="tab-content">
+    //             <h2>Contact</h2>
+    //             <p>Email: <a href="mailto:sayanc3@illinois.edu">sayanc3@illinois.edu</a></p>
+    //             <p>Email: <a href="mailto:sayanc2012@gmail.com">sayanc2012@gmail.com</a></p>
+    //         </section>
+    //     `
+    // };
 
     // Add click event listeners to tabs
     tabs.forEach(tab => {
